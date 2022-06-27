baeeaa5e619f5b3b7b17bab611789ab5dd013f82
DribDrob <drobushenko@gmail.com>
2022-06-27 19:27:50 +0300

    styled css modules all

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,7 +11,7 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
         <p className={css.location}>{location}</p>
       </div>
 
-      <ul className={css.stats}>
+      <ul className={css.statsList}>
         <li>
           <span className={css.label}>Followers</span>
           <span className={css.quantity}>{stats.followers}</span>
